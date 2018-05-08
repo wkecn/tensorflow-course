608fe2284ea91745fcb821997a88c4d7fb02a4c9
wangkechn <wangkechn@aliyun.com>
2018-05-08 17:44:50 +0800

    code

---
+++ 第九节/retain/retrain.bat
@@ -0,0 +1,8 @@
+python C:\Users\wangkechn\Workspace-tensorflow\tensorflow-study/retrain.py ^
+--bottleneck_dir bottleneck ^
+--how_many_training_steps 200 ^
+--model_dir D:/Tensorflow/inception_model/ ^
+--output_graph output_graph.pb ^
+--output_labels output_labels.txt ^
+--image_dir data/train/
+pause
